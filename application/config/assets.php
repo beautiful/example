40b71b3be0af1aaac0886dc2dcbc176f27fd9e08
Luke Morton <lukemorton.designs@gmail.com>
2011-07-24 20:59:01 +0100

    Add config for assets.

--- application/config/assets.php
+++ application/config/assets.php
@@ -4,9 +4,21 @@ return array(
 
 	'groups' => array(
 		'stylesheets' => array(
-			array('css', 'cool/example.css'),
-			array('css', 'cool/example.css'),
+			array('css', 'css/screen.css'),
+			array('css', 'css/print.css', array(
+					'media' => 'print',
+				)),
+		),
+		'javascripts' => array(
+			array('js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js'),
+			array('js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js'),
+			array('js', 'js/app.js'),
 		),
 	),
-
+	
+	'filters' => array(
+		'stylesheets' => array(array('Concat')),
+		'javascripts' => array(array('Concat'), array('JsMinPlus')),
+	),
+	
 );
